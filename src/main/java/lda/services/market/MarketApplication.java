5ac77d42c646a237e07f92c45189a4f9f1a23215
dauresl <lolo>
2026-01-14 22:33:55 +0100

    init

---
+++ src/main/java/lda/services/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package lda.services.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
+
+}
